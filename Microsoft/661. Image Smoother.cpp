3529571e9c806eacf12a2dc2ff681364102c2858
Astha Sahani <142304973+Astha86@users.noreply.github.com>
2025-01-02 20:51:50 +0530

    added files via upload

---
+++ Microsoft/661. Image Smoother.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    int helper(vector<vector<int>>& v, int i, int j, int r, int c){
+        int up = max(0, i-1);
+        int dwn = min(r, i+2);
+        int lft = max(0, j-1);
+        int rgt = min(c, j+2);
+
+        int sum = 0;
+        int cnt = 0;
+
+        for(int row=up; row<dwn; row++){
+            for(int col=lft; col<rgt; col++){
+                sum += v[row][col];
+                cnt++;
+            }
+        }
+
+        return sum/cnt;
+    }
+
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int r = img.size();
+        int c = img[0].size();
+
+        vector<vector<int>> ans(r, vector<int>(c,0));
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                ans[i][j] = helper(img, i, j, r, c);
+            }
+        }
+
+        return ans;
+    }
+};
