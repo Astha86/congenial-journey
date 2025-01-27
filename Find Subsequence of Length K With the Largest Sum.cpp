a39bd3016d22b3d8d5dd03de8f0ba67a09d0d7a7
Astha Sahani <142304973+Astha86@users.noreply.github.com>
2025-01-27 23:39:29 +0530

    Added LeetCode 2099

--- Find Subsequence of Length K With the Largest Sum.cpp
+++ Find Subsequence of Length K With the Largest Sum.cpp
@@ -3,12 +3,14 @@ public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
         int n = nums.size();
         vector<pair<int, int>> v;
+        
         for(int i=0; i<n; i++){
             v.push_back({nums[i], i});
         }
 
         sort(v.begin(),v.end()); // sorting the numbers
         vector<pair<int, int>> v2;
+        
         int i=n-1;
         int count = 0;
         while(count!=k){
@@ -19,9 +21,11 @@ public:
 
         sort(v2.begin(),v2.end()); // sorting the idices
         vector<int>ans;
+        
         for(int i=0; i<v2.size(); i++){
             ans.push_back(nums[v2[i].first]);
         }
+        
         return ans;
     }
 };
